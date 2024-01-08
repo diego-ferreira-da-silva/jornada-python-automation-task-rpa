d1fa0759df8bf600f1c50f7f218c3c30fa5990c8
Diego Ferreira <diego_fsilva92@hotmail.com>
2024-01-08 20:38:30 -0300

    interação com login em formilário

---
+++ auxiliar.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+time.sleep(3)
+print(pyautogui.position())
+
+
+#Point(x=743, y=427) e-mail
+#Point(x=709, y=475) senha
+#Point(x=965, y=532) botão logar
--- codigo.py
+++ codigo.py
@@ -10,5 +10,10 @@ link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(link)
 
 pyautogui.press("enter")
-
-time.sleep(5)
+time.sleep(3)
+pyautogui.click(x=735, y=373)
+pyautogui.write("emailfake@email.com")
+pyautogui.press("tab")
+pyautogui.write("senhafake")
+pyautogui.click(x=965, y=532)
+time.sleep(3)
