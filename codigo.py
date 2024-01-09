392031f2c58a7ae0a363080c6c46d0d8ddd9d520
Diego Ferreira <diego_fsilva92@hotmail.com>
2024-01-09 08:51:17 -0300

    código final da automação parametrizado

--- codigo.py
+++ codigo.py
@@ -2,14 +2,8 @@ import pyautogui
 import pandas
 import time
 
-
-def tecla(tecla):
-    teclapress = pyautogui.press(tecla)
-    return teclapress
-
-
 # Abrindo Chrome
-pyautogui.PAUSE = 1
+pyautogui.PAUSE = 0.3
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
@@ -18,7 +12,6 @@ pyautogui.press("enter")
 link = "https://dlp.hashtagtreinamentos.com/python/intensivao/login"
 pyautogui.write(link)
 pyautogui.press("enter")
-time.sleep(3)
 
 # preenchendo formulário login
 pyautogui.click(x=735, y=373)
@@ -26,7 +19,6 @@ pyautogui.write("emailfake@email.com")
 pyautogui.press("tab")
 pyautogui.write("senhafake")
 pyautogui.click(x=965, y=532)
-time.sleep(10)
 
 # verificando tabela produtos.csv
 tabela = pandas.read_csv("produtos.csv")
@@ -37,29 +29,30 @@ tabela = pandas.read_csv("produtos.csv")
 for linha in tabela.index:
   pyautogui.click(x=743, y=256)
 
-  pyautogui.write("MOLO000251")
+  pyautogui.write(tabela.loc[linha, "codigo"])
   pyautogui.press("tab")
 
-  pyautogui.write("Logitech")
+  pyautogui.write(tabela.loc[linha, "marca"])
   pyautogui.press("tab")
 
-  pyautogui.write("Mouse")
+  pyautogui.write(tabela.loc[linha, "tipo"])
   pyautogui.press("tab")
 
-  pyautogui.write("1")
+  pyautogui.write(str(tabela.loc[linha, "categoria"]))
   pyautogui.press("tab")
 
-  pyautogui.write("25.95")
+  pyautogui.write(str(tabela.loc[linha, "preco_unitario"]))
   pyautogui.press("tab")
 
-  pyautogui.write("6.5 ")
+  pyautogui.write(str(tabela.loc[linha, "custo"]))
   pyautogui.press("tab")
 
-  pyautogui.write("")
-  pyautogui.press("tab")
-
+  obs = tabela.loc[linha, "obs"]
+  if pandas.isna(obs):
+    pyautogui.write(obs)
+    pyautogui.press("tab")
+    
   pyautogui.press("tab")
   pyautogui.press('enter')
 
   pyautogui.screenshot(5000)
-
